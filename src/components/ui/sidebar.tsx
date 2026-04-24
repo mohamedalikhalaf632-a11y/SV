3edb73ecb6d419a2c52cda6a95353b5222a82987
mohamedalikhalaf632-a11y <mohamedalikhalaf632@gmail.com>
2026-04-24 23:49:44 +0300

    ss

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -210,15 +210,6 @@ const Sidebar = React.forwardRef<
         >
           {children}
         </div>
-        <div className="flex flex-col h-screen">
-        <div className="flex-1 overflow-y-auto">
-        {/* content */}
-        </div>
-
-          <button className="p-4">
-             Logout
-          </button>
-        </div>
       </div>
     </div>
   );
