dae2795729c05b19051a42e5b5de0892c5e63db9
mohamedalikhalaf632-a11y <mohamedalikhalaf632@gmail.com>
2026-04-25 16:54:43 +0300

    kk

--- src/pages/sitemap.xml.js
+++
@@ -1,8 +0,0 @@
-export default function Sitemap() {}
-
-export async function getServerSideProps({ res }) {
-  res.setHeader('Content-Type', 'text/xml');
-  res.write('<?xml version="1.0" encoding="UTF-8"?><urlset><url><loc>https://silentvoice-virid.vercel.app</loc></url></urlset>');
-  res.end();
-  return { props: {} };
-}
